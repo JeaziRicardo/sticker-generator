4cb40f6e247020f64ad7f35c97168832597aa559
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-04-02 22:05:54 -0300

    feat: atribui em inputImage a imagem da internet

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.io.InputStream;
+import java.net.URL;
+
 public class App {
     public static void main(String[] args) throws Exception {
         StickerGenerator generator = new StickerGenerator();
+        InputStream inputImage = 
+                        new URL("https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@.jpg")
+                        .openStream();
     }
 }
