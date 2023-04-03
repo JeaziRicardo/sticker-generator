897c0a07fbcacb73a04113cf025093a0a104629b
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2023-04-02 21:40:46 -0300

    feat: ler a imagem original

--- src/StickerGenerator.java
+++ src/StickerGenerator.java
@@ -1,5 +1,14 @@
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
 public class StickerGenerator {
     
-    public void generate() throws Exception {
+
+    public void generate(InputStream inputImage) throws Exception {
+
+        BufferedImage originalImg = ImageIO.read(inputImage);
+
     }
 }
